0bb6c682c22086ad1f68092764acc6198e3fb4f8
Rloxx <133797862+Rloxx@users.noreply.github.com>
2023-06-22 16:40:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import asyncio
+import os
+os.system("pip install aiohttp")
+import aiohttp
+os.system("pip install tasksio")
+import sys
+import colorama
+from colorama import Fore
+import tasksio
+import asyncio
+from typing import Optional
+
+def setTitle(title: Optional[any]=None):
+  os.system("title "+title)
+
+setTitle("Server Booster - [RedCrystal Inviter]")
+
+def clear():
+  if sys.platform in ["linux", "linux2"]:
+    os.system("clear")
+  else:
+    os.system("cls")
+clear()
+
+async def join_server(token, inv):
+  headers = {"Authorization": token, "accept": "*/*", "accept-language": "en-US", "connection": "keep-alive", "cookie": f'__cfduid={os.urandom(43).hex()}; __dcfduid={os.urandom(32).hex()}; locale=en-US', "DNT": "1", "origin": "https://discord.com", "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-origin", "referer": "https://discord.com/channels/@me", "TE": "Trailers", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9001 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36", "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAxIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDIiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODMwNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"}
+  async with aiohttp.ClientSession(headers=headers) as serverjoinersession:
+    async with serverjoinersession.post(f"https://discord.com/api/v9/invites/{inv}") as response:
+      if response.status in (204, 200, 201):
+        print(f"[-] RedCrystal | Successfully Joined Server")
+      else:
+        print(f"[-] RedCrystal | Failed To Join Server, Status Code: {response.status}")
+
+async def boost_server(guildid, token):
+  headers = {"Authorization": token, "accept": "*/*", "accept-language": "en-US", "connection": "keep-alive", "cookie": f'__cfduid={os.urandom(43).hex()}; __dcfduid={os.urandom(32).hex()}; locale=en-US', "DNT": "1", "origin": "https://discord.com", "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-origin", "referer": "https://discord.com/channels/@me", "TE": "Trailers", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9001 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36", "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAxIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDIiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODMwNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"}
+  async with aiohttp.ClientSession(headers=headers) as ClientSession:
+    async with ClientSession.get(f"https://discord.com/api/v9/users/@me/guilds/premium/subscription-slots") as nvmmm:
+      if nvmmm.status == 200:
+        idk_var = await nvmmm.json()
+        for varr in idk_var:
+          id__ = varr['id']
+          payload = {"user_premium_guild_subscription_slot_ids": [id__]}
+          async with ClientSession.put(f"https://discord.com/api/v9/guilds/{guildid}/premium/subscriptions", json=payload) as boost_req:
+            btxt = await boost_req.text()
+            if "id" in btxt:
+              
+              print(f"[-] RedCrystal | Successfully Boosted Server")
+            else:
+              print("[-] RedCrystal | Failed To Boost Server, Unknown Error Occurred")
+
+
+banner = f"""{Fore.RED}[-]{Fore.RESET} Created By Rloxx\n\n{Fore.BLUE}[1]{Fore.RESET} Server Joiner\n\n{Fore.BLUE}[2]{Fore.RESET} Boost Server\n"""
+
+
+async def start_join(inv):
+  async with tasksio.TaskPool(10_000) as pool:
+    for token in open('tokens.txt', 'r').readlines():
+      tk = token.strip()
+      await pool.put(join_server(tk, inv))
+
+async def start_boost(id):
+  async with tasksio.TaskPool(10_000) as pool:
+    for token in open('tokens.txt', 'r').readlines():
+      tk = token.strip()
+      await pool.put(boost_server(id, tk))
+  
+
+print(banner)
+ch = input("[-] RedCrystal | Choice: ")
+try:
+  c = int(ch)
+except ValueError:
+  print("RedCrystal | Use Number To Choose.")
+  sys.exit()
+  
+if c == 1:
+  invv = input("[-] RedCrystal | Enter Invite Code: discord.gg/")
+  asyncio.run(start_join(invv))
+elif c == 2:
+  g = int(input("[-] RedCrystal | Enter Guild ID: "))
+  asyncio.run(start_boost(g))
+else:
+  print("[-] RedCrystal | Invaild Option")
+  exit(0)
